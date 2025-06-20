eeaf3e816217a416a188022ed1e977e4c4c03b5d
chandanshyam <pchandan@colostate.edu>
2025-06-20 17:41:06 -0600

    react-frontend and README.md

---
+++ fitness-Ai-frontend/src/App.jsx
@@ -0,0 +1,79 @@
+import { Box, Button } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "react-oauth2-code-pkce";
+import { useDispatch } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router";
+import { setCredentials } from "./store/authSlice";
+import ActivityForm from "./compnents/ActivityForm";
+import ActivityList from "./compnents/ActivityList";
+import ActivityDetail from "./compnents/ActivityDetail";
+
+const ActivitiesPage = () => {
+  return (
+    <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
+      <ActivityForm onActivitiesAdded={() => window.location.reload} />
+      <ActivityList />
+    </Box>
+  );
+};
+function App() {
+  const { token, tokenData, login, logout, isAuthenticated } =
+    useContext(AuthContext);
+  const dispatch = useDispatch();
+  const [authReady, setauthReady] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(setCredentials({ token, user: tokenData }));
+      setauthReady(true);
+    }
+  }, [token, tokenData, dispatch]);
+
+  return (
+    <Router>
+      {!token ? (
+        <Button
+          variant="contained"
+          color="#dc004e"
+          onClick={() => {
+            login();
+          }}
+        >
+          {" "}
+          Login
+        </Button>
+      ) : (
+        <div>
+          {
+            /* <pre>{JSON.stringify(tokenData, null, 2)}</pre> */
+
+            <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
+              <Routes>
+                <Route path="/activities" element={<ActivitiesPage />} />
+                <Route path="/activities/:id" element={<ActivityDetail />} />
+                <Route
+                  path="/"
+                  element={
+                    token ? (
+                      <Navigate to="/activities" replace />
+                    ) : (
+                      <div>Welcome! Please Login</div>
+                    )
+                  }
+                />
+              </Routes>
+            </Box>
+          }
+        </div>
+      )}
+    </Router>
+  );
+}
+
+export default App;
